904586538c8c8c7325cfd6ed8c58ef9b1f412325
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-12 16:21:08 +0530

    Create user details.c

---
+++ user details.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int Age;
+float weight,hight;
+char Name,college,USN;
+int main(){
+    printf("Enter the college name:",college);
+    scanf("%s",&college);
+    printf("Enter name:",Name);
+    scanf("%s",&Name);
+    printf("Enter the USN:",USN);
+    scanf("%s",&USN);
+    printf("Age:",Age);
+    scanf("%d",&Age);
+    printf("hight:",hight);
+    scanf("%f",&hight);
+    printf("weight:",weight);
+    scanf("%f",&weight);
+    
+}
