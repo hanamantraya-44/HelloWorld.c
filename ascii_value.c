6848f4f88b6fca12aa9554143b30b7c259228e47
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-13 16:10:48 +0530

    Create ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the values(both upper and lower case):");
+    scanf("%c",&ch);
+    printf("The ASCII value of %c is:%d",ch,ch);
+    return 0;
+}
+
