29c79c636bcc6984c291c18e94924b2a0011eee6
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-13 15:53:24 +0530

    Create sheidevi_fun.c

---
+++ sheidevi_fun.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int shridevi(){
+printf("Hanamantraya DG\n");
+printf("Age:18\n");
+printf("09/10/2006\n");
+printf("ph.no:9876543210\n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
