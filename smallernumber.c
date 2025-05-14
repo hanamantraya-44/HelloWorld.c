e5e695340c849967382043e7ab3cbd6930237977
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-14 09:12:04 +0530

    Create smallernumber.c

---
+++ smallernumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c;
+printf("Enter the value of a,b,c:");
+scanf("%d%d%d",&a,&b,&c);
+if(a<b&&a<c)
+{
+    printf("%d is smaller",a);
+}
+else if(b<a&&b<c){
+    printf("%d is smaller",b);
+}
+else{
+    printf("%d is smaller",c);
+}
+return 0;
+}
