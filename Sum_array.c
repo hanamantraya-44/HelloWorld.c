e93583bbee6a0bfd09f637d8739d6dce74c3bb49
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-14 15:36:51 +0530

    Create Sum_array.c

---
+++ Sum_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n, are[n], sum=0, i;
+   printf ("enter the value of n:");
+   scanf("%d",&n);
+   printf ("enter the elements\n");
+   for(i=0;i<n;i++){
+       scanf("%d",&are[n]);
+   sum+=are[n];
+   }
+   printf("%d",sum);
+}
