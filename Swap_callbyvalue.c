9e6e8d769b210f49b0b545583b14f360826c1502
hanamantraya-44 <hanamantrayagurikar140@gmail.com>
2025-05-15 13:06:17 +0530

    Create Swap_callbyvalue.c

---
+++ Swap_callbyvalue.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int x=10, y=20,z;
+    printf ("Before swap, x=%d y=%d\n",x,y);
+    z=x;
+    x=y;
+    y=z;
+    printf ("After swapping, x=%d y=%d",x,y);
+    
+}
